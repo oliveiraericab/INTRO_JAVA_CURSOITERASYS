21f253b99daa5644f68be0c703a3d257104e60cd
oliveiraericab <oliveiraericab@gmail.com>
2022-12-27 23:55:39 -0300

    Início.

---
+++ src/main/java/org/example/Vendas.java
@@ -0,0 +1,18 @@
+//Classe aberta em Main > Nem Class.
+// Qual a diferença de abrir assim ou apenas digitar na aba anterior build.gradle(INTRO*)
+
+// 1. Pacote
+package org.example;
+
+// 2. Referência às bibliotecas
+
+// 3. Classes
+public class Vendas {
+    // 3.1 Atributos - caracteristicas
+    // 3.2 Metodos e funcoes
+
+    public static void main(Stringln[] args) {
+        System.out.println(Minha primeira função executada!);
+    }
+
+}
