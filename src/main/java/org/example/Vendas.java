c267e17cbec523c0b5cc61826a6fd90b7db6c34e
oliveiraericab <oliveiraericab@gmail.com>
2023-01-07 12:59:51 -0300

    Adicionei um método com a condicional switch para escolher entre os métodos : ifsimples, curto, extenso e default.

--- src/main/java/org/example/Vendas.java
+++ src/main/java/org/example/Vendas.java
@@ -9,11 +9,30 @@ package org.example;
 // 3. Classes
 public class Vendas {
     // 3.1 Atributos - caracteristicas
-
-
     // 3.2 Metodos e funcoes
     public static void main(String[] args) {
-    // Condicional é verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou um software.
+        // Condicional é verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou um software.
+        // Switch - selecionar o comportamento do programa conforme a escolha do usuário ou software.
+        String opcao = "curto";
+        switch (opcao) {
+            case "ifSimples":
+                System.out.println("Você escolheu executar o método ifSimples");
+                ifSimples();
+                break;
+            case "curto":
+                System.out.println("Você escolheu executar o método calculoTotalCodigoCurto");
+                calculoTotalCodigoCurto();
+                break;
+            case "extenso":
+                System.out.println("Você escolheu executar o método calculoTotalCodigoExtenso");
+                calculoTotalCodigoExtenso();
+                break;
+            default:
+                System.out.println("Você escolheu uma opção que não está associanda a nenhum método no sistema.");
+        }
+        }
+    public static void ifSimples(){
+        // Condicional é verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou um software.
         // if - se
         // else - senão
         String modo = "curto";
@@ -23,15 +42,13 @@ public class Vendas {
         else {
             calculoTotalCodigoExtenso();
         }
-        }
-
+    }
     public static void calculoTotalCodigoCurto(){
         System.out.println("Calcule o valor total da compra: (reduzido)");
         // Cálculo de total código reduzido
         int quantidade = 3;
         int valorunitario = 5;
         System.out.println( "Para compra de " + quantidade + " itens que custam " + valorunitario + " reais cada, o valor total da compra é " + quantidade * valorunitario + " reais." );
-
     }
     public static void calculoTotalCodigoExtenso(){
         System.out.println("Calcule o valor total da compra: (extenso)");
@@ -40,11 +57,9 @@ public class Vendas {
         int quantidade;
         int valorunitario;
         int total;
-
         quantidade = 3;
         valorunitario = 5;
         total = quantidade * valorunitario;
-
         System.out.println( "Para compra de " + quantidade + " itens que custam " + valorunitario + " reais cada, o valor total da compra é " + total + " reais." );
     }
 }
